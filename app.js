35ad54ea0c77fbad86b4426513c95053ba80870c
zconn08 <zconn@unknowna8bbcf21d658.gateway.pace.com>
2014-07-14 23:27:53 -0700

    Final Draft

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-$(document).ready(function(){
-
-//Find out number in textbox
-$(".submit").click(function(){
-	var a = $("input").val();
 //Fizzbuzz Function
+var fizzBuzz = function(a){
 for (var i = 1; i <= a; i++) {
 	if (i%15===0) {
-		$("#listContainer").append("<li>fizzbuzz</li>");
+		$("#listContainer").append("<li>fizz buzz</li>");
 	}
 	else if (i%5===0){
 		$("#listContainer").append("<li>buzz</li>");
@@ -17,6 +13,19 @@ for (var i = 1; i <= a; i++) {
 	else {
 		$("#listContainer").append("<li>" + i + "</li>");
 	}
+}
+};
+
+$(document).ready(function(){
+
+//Find out number in textbox
+$(".submit").click(function(){
+	var num = $("input").val();
+
+if (num % 1 === 0) {
+	fizzBuzz(num);
+} else{
+	alert("Please enter a whole number!");
 }
 });
 });
